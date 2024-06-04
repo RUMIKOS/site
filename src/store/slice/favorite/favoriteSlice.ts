f8441e552cfd438a40aea752757efe04cc6a200d
Leonid Frolov <rumikosumiko@gmail.com>
2024-06-04 22:26:50 +0300

    xd

--- src/store/slice/favorite/favoriteSlice.ts
+++ src/store/slice/favorite/favoriteSlice.ts
@@ -1,60 +1,32 @@
-// import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-// type fav ={
-// 	anime:{}
-// }
-// type favoriteState = {
-// 	favorite:fav[]
-// }
+type fav ={
+	anime:{}
+}
+type favoriteState = {
+	favorite:fav[]
+}
 
-// const initialState :favoriteState={
-// 	favorite:[]
-// }
-
-// const favoriteSlice = createSlice({
-// 	name: 'favorite',
-// 	initialState,
-// 	reducers: {
-// 		addFavorite(state, action:PayloadAction<{}>) {
-// 			state.favorite.push({
-// 				anime: action.payload,
-// 			})
-// 		},
-// 		removeFavorite(state, action:PayloadAction<{}>) {
-
-// 			console.log(action.payload);
-// 			state.favorite = state.favorite.filter(el => el.anime.id !== action.payload.anime.id)
-// 		},
-// 	},
-// })
-
-// export const { addFavorite , removeFavorite } = favoriteSlice.actions
-// export default favoriteSlice.reducer
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
-
-
-const initialState = 
-	[]
+const initialState :favoriteState={
+	favorite:[]
+}
 
 const favoriteSlice = createSlice({
 	name: 'favorite',
 	initialState,
 	reducers: {
-		addFavorite(state, action) {
-			console.log(action.payload);
-			
-			state.push(
-				action.payload,
-			)
+		addFavorite(state, action:PayloadAction<{}>) {
+			state.favorite.push({
+				anime: action.payload,
+			})
 		},
-		removeFavorite(state, action: PayloadAction<{}>) {
-			console.log(action.payload)
-			state.favorite = state.favorite.filter(
-				el => el.anime.id !== action.payload.anime.id
-			)
+		removeFavorite(state, action:PayloadAction) {
+			dasdsa
+			console.log(action.payload);
+			state.favorite = state.favorite.filter(el => el.anime.id !== action.payload.anime.id)
 		},
 	},
 })
 
-export const { addFavorite, removeFavorite } = favoriteSlice.actions
+export const { addFavorite , removeFavorite } = favoriteSlice.actions
 export default favoriteSlice.reducer
